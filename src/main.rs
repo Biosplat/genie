7fd8d9ab0e3387ddf67de3d840c1d8644425ad80
Biosplat <huxonli@gmail.com>
2024-02-21 14:15:52 -0500

    simple server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-fn main() {
-    
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
 
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> Html<String> {
+    Html(
+        std::fs::read_to_string("frontend/index.html")
+        .expect("no index.html exists")
+    )
 }
